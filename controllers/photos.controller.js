0f0fcb73e9daec2c35ee7bb0eaf25d8c8903f39c
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-08-10 11:47:08 +0200

    Add conditions to check correctness of filled fields

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,56 +1,81 @@
-const Photo = require('../models/photo.model');
+const Photo = require("../models/photo.model");
 
 /****** SUBMIT PHOTO ********/
 
 exports.add = async (req, res) => {
-
   try {
     const { title, author, email } = req.fields;
     const file = req.files.file;
 
-    if(title && author && email && file) { // if fields are not empty...
+    if (title && author && email && file) {
+      // if fields are not empty...
 
-      const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
-      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
-      await newPhoto.save(); // ...save new photo in DB
-      res.json(newPhoto);
+      const authorPattern = new RegExp(
+        /(<\s*(strong|em)*>(([A-z]|\s)*)<\s*\/\s*(strong|em)>)|(([A-z]|\s|\.)*)/,
+        "g"
+      );
+      const emailPattern = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "g");
 
+      const authorMatched = author.match(authorPattern).join("");
+      const emailMatched = email.match(emailPattern).join("");
+
+      if (authorMatched.length < author.length) {
+        throw new Error("Author is not valid");
+      } else if (emailMatched.length < email.length) {
+        throw new Error("Email is not valid");
+      }
+
+      const fileName = file.path.split("/").slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
+      const fileExt = fileName.split(".").slice(-1)[0];
+
+      if (
+        (fileExt === "jpg" || "png" || "gif") &&
+        title.length <= 25 &&
+        author.length <= 50
+      ) {
+        console.log(fileExt);
+        const newPhoto = new Photo({
+          title,
+          author,
+          email,
+          src: fileName,
+          votes: 0,
+        });
+        await newPhoto.save(); // ...save new photo in DB
+        res.json(newPhoto);
+      } else {
+        throw new Error("Wrong file type!");
+      }
     } else {
-      throw new Error('Wrong input!');
+      throw new Error("Wrong input!");
     }
-
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
-
 };
 
 /****** LOAD ALL PHOTOS ********/
 
 exports.loadAll = async (req, res) => {
-
   try {
     res.json(await Photo.find());
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
-
 };
 
 /****** VOTE FOR PHOTO ********/
 
 exports.vote = async (req, res) => {
-
   try {
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
-    if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
+    if (!photoToUpdate) res.status(404).json({ message: "Not found" });
     else {
       photoToUpdate.votes++;
       photoToUpdate.save();
-      res.send({ message: 'OK' });
+      res.send({ message: "OK" });
     }
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
-
 };
