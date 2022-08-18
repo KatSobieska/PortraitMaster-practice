24045abb68ec1e52002bb1a87a68ebd26bc6c651
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-08-18 07:36:45 +0200

    Update app.get path

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(express.static(path.join(__dirname, "/client/build")));
 // add photo routes
 app.use("/api", require("./routes/photos.routes"));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
